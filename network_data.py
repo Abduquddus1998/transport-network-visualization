d910b3c5f40b8615b5ccf21160bdf3eaf7e35075
Abduquddus Azizov <abduquddus.08@mail.ru>
2023-11-06 14:07:39 +0000

    initialised underground network data

---
+++ network_data.py
@@ -0,0 +1,94 @@
+# Sweden underground line "T19"
+t19_line = [{"name": "Hagsätra", "short_name": "Hag", "color": "#34a981", "pos": (82, -16), "label_pos": (60, -18)},
+            {"name": "Stureby", "short_name": "Stu", "color": "#34a981", "pos": (110, 0), "label_pos": (105, -13)},
+            # {"name": "Gullmarsplan", "short_name": "Gul", "color": "#34a981", "pos": (135, 15), "label_pos": (142,
+            # 15)},
+            {"name": "Slussen", "short_name": "Slu", "color": "#610b2e", "pos": (135, 25), "label_pos": None},
+            {"name": "Gamla stan", "short_name": "Gam", "color": "#610b2e", "pos": (135, 75), "label_pos": None},
+            {"name": "Odenplan", "short_name": "Ode", "color": "#34a981", "pos": (110, 75), "label_pos": (102, 87)},
+            {"name": "Fridhemsplan", "short_name": "Fri", "color": "#154564", "pos": (85, 75), "label_pos": None},
+            {"name": "Alvik", "short_name": "Alv", "color": "#34a981", "pos": (60, 75), "label_pos": (57, 87)},
+            {"name": "Åkeshov strand", "short_name": "Åke", "color": "#34a981", "pos": (40, 75), "label_pos": (30, 87)},
+            {"name": "Hässelby strand", "short_name": "Häs", "color": "#34a981", "pos": (5, 75), "label_pos": (-5, 87)}]
+
+# Sweden underground line "T18"
+t18_line = [
+    {"name": "Farsta strand", "short_name": "Fa", "color": "#077550", "pos": (135, -60), "label_pos": (108, -64)},
+    {"name": "Tallkrogen", "short_name": "Tal", "color": "#077550", "pos": (135, -25), "label_pos": (108, -30)},
+    # {"name": "Gullmarsplan", "short_name": "Gul", "color": "#34a981", "pos": (135, 15), "label_pos": None},
+    {"name": "Slussen", "short_name": "Slu", "color": "#610b2e", "pos": (135, 25), "label_pos": None},
+    {"name": "Gamla stan", "short_name": "Gam", "color": "#610b2e", "pos": (135, 75), "label_pos": None},
+    {"name": "Odenplan", "short_name": "Ode", "color": "#34a981", "pos": (110, 75), "label_pos": None},
+    {"name": "Fridhemsplan", "short_name": "Fri", "color": "#154564", "pos": (85, 75), "label_pos": None},
+    {"name": "Alvik", "short_name": "Alv", "color": "#34a981", "pos": (60, 75), "label_pos": None}]
+
+# Sweden underground line "T17"
+t17_line = [{"name": "Skarpnäck", "short_name": "Ska", "color": "#527D6E", "pos": (175, -45), "label_pos": (177, -45)},
+            {"name": "Björkhagen", "short_name": "Bjö", "color": "#527D6E", "pos": (155, -8), "label_pos": (162, -8)},
+            # {"name": "Gullmarsplan", "short_name": "Gul", "color": "#34a981", "pos": (135, 15), "label_pos": None},
+            {"name": "Slussen", "short_name": "Slu", "color": "#610b2e", "pos": (135, 25), "label_pos": None},
+            {"name": "Gamla stan", "short_name": "Gam", "color": "#610b2e", "pos": (135, 75), "label_pos": None},
+            {"name": "Odenplan", "short_name": "Ode", "color": "#34a981", "pos": (110, 75), "label_pos": None},
+            {"name": "Fridhemsplan", "short_name": "Fri", "color": "#154564", "pos": (85, 75), "label_pos": None},
+            {"name": "Alvik", "short_name": "Alv", "color": "#34a981", "pos": (60, 75), "label_pos": None},
+            {"name": "Åkeshov strand", "short_name": "Åke", "color": "#34a981", "pos": (40, 75), "label_pos": None}]
+
+# Sweden underground line "T14"
+t14_line = [{"name": "Fruängen", "short_name": "Fru", "color": "#f10d6b", "pos": (45, -5), "label_pos": (24, -6)},
+            {"name": "Liljeholmen", "short_name": "Lil", "color": "#610b2e", "pos": (60, 25), "label_pos": None},
+            {"name": "Zinkensdamm", "short_name": "Zin", "color": "#610b2e", "pos": (100, 25), "label_pos": None},
+            {"name": "Slussen", "short_name": "Slu", "color": "#610b2e", "pos": (135, 25), "label_pos": None},
+            {"name": "Gamla stan", "short_name": "Gam", "color": "#610b2e", "pos": (135, 75), "label_pos": None},
+            # {"name": "Östermalmstorg", "short_name": "Öst", "color": "#610b2e", "pos": (135, 100),
+            #  "label_pos": None},
+            {"name": "Universitetet", "short_name": "Uni", "color": "#f10d6b", "pos": (135, 120),
+             "label_pos": (107, 120)},
+            {"name": "Mörby centrum", "short_name": "Mör", "color": "#f10d6b", "pos": (135, 160),
+             "label_pos": (105, 160)}]
+
+# Sweden underground line "T13"
+t13_line = [{"name": "Norsborg", "short_name": "Nor", "color": "#610b2e", "pos": (10, -15), "label_pos": (-12, -16)},
+            {"name": "Axelsberg", "short_name": "Axe", "color": "#610b2e", "pos": (30, 25), "label_pos": (22, 35)},
+            {"name": "Liljeholmen", "short_name": "Lil", "color": "#610b2e", "pos": (60, 25), "label_pos": (50, 35)},
+            {"name": "Zinkensdamm", "short_name": "Zin", "color": "#610b2e", "pos": (100, 25), "label_pos": (90, 35)},
+            {"name": "Slussen", "short_name": "Slu", "color": "#610b2e", "pos": (135, 25), "label_pos": (142, 37)},
+            {"name": "Gamla stan", "short_name": "Gam", "color": "#610b2e", "pos": (135, 75), "label_pos": (142, 75)},
+            # {"name": "Östermalmstorg", "short_name": "Öst", "color": "#610b2e", "pos": (135, 130),
+            #  "label_pos": (110, 127)},
+            {"name": "Ropsten", "short_name": "Rop", "color": "#610b2e", "pos": (160, 140), "label_pos": (168, 138)}]
+
+# Sweden underground line "T11"
+t11_line = [{"name": "Akalla", "short_name": "Aka", "color": "#268ccf", "pos": (45, 160), "label_pos": (52, 160)},
+            {"name": "Kista", "short_name": "Kis", "color": "#268ccf", "pos": (62, 135), "label_pos": (69, 135)},
+            {"name": "Stadshagen", "short_name": "Sta", "color": "#154564", "pos": (80, 110), "label_pos": None},
+            {"name": "Fridhemsplan", "short_name": "Fri", "color": "#154564", "pos": (85, 75), "label_pos": None},
+            {"name": "Rådhuset", "short_name": "Råd", "color": "#154564", "pos": (110, 50), "label_pos": None},
+            {"name": "Kungsträdgården", "short_name": "Kun", "color": "#154564", "pos": (180, 55),
+             "label_pos": None}]
+
+# Sweden underground line "T10"
+t10_line = [
+    {"name": "Hjulsta", "short_name": "Hju", "color": "#154564", "pos": (0, 160), "label_pos": (8, 160), "weight": 10},
+    {"name": "Duvbo", "short_name": "Duv", "color": "#154564", "pos": (20, 110), "label_pos": (18, 122)},
+    {"name": "Huvudsta", "short_name": "Huv", "color": "#154564", "pos": (50, 110), "label_pos": (45, 122)},
+    {"name": "Stadshagen", "short_name": "Sta", "color": "#154564", "pos": (80, 110), "label_pos": (87, 110)},
+    {"name": "Fridhemsplan", "short_name": "Fri", "color": "#154564", "pos": (85, 75), "label_pos": (73, 61)},
+    {"name": "Rådhuset", "short_name": "Råd", "color": "#154564", "pos": (110, 50), "label_pos": (88, 48)},
+    {"name": "Kungsträdgården", "short_name": "Kun", "color": "#154564", "pos": (180, 50),
+     "label_pos": (168, 35)}]
+
+network_dict = {"t14_line": t14_line, "t13_line": t13_line, "t19_line": t19_line, "t18_line": t18_line,
+                "t17_line": t17_line, "t11_line": t11_line, "t10_line": t10_line}
+
+edge_colors = {
+    "t19_line": "#34a981", "t18_line": "#077550", "t17_line": "#527D6E", "t14_line": "#f10d6b",
+    "t13_line": "#610b2e", "t11_line": "#268ccf", "t10_line": "#154564"
+}
+
+network_line_labels = [{"name": "T13", "pos": (5, -35), "color": "red"},
+                       {"name": "T14", "pos": (38, -25), "color": "red"},
+                       {"name": "T19", "pos": (74, -33), "color": "red"},
+                       {"name": "T18", "pos": (129, -80), "color": "red"},
+                       {"name": "T17", "pos": (169, -65), "color": "red"},
+                       {"name": "T11", "pos": (38, 172), "color": "red"},
+                       {"name": "T10", "pos": (-5, 172), "color": "red"}]
