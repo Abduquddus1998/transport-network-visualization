ad288b5c3567ccf6bc06b16343bfd69d2d55bec5
Abduquddus Azizov <abduquddus.08@mail.ru>
2023-11-06 17:40:04 +0000

    stockholm underground visualisation completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from network_data import network_dict, edge_colors, network_line_labels
+from network_data import network_dict, edge_colors, network_line_labels, edge_weights
 
 network_graph = nx.Graph()
 
@@ -21,6 +21,15 @@ def add_network_nodes():
 add_network_nodes()
 
 
+def get_edge_weight(from_none, to_node):
+    for edge_weight in iter(edge_weights):
+
+        if from_none == edge_weight["from"] and to_node == edge_weight["to"]:
+            return edge_weight["weight"]
+
+    return 0
+
+
 def add_network_edges():
     for line in iter(network_dict):
         network_line = network_dict[line]
@@ -28,10 +37,13 @@ def add_network_edges():
 
         for i in range(0, len(network_line) - 1):
             next_node = i + 1
+
             current_node = network_line[i]
             next_node = network_line[next_node]
 
-            network_graph.add_edge(current_node["short_name"], next_node["short_name"], color=line_color, weight=10)
+            weight = get_edge_weight(current_node["short_name"], next_node["short_name"])
+
+            network_graph.add_edge(current_node["short_name"], next_node["short_name"], color=line_color, weight=weight)
 
 
 add_network_edges()
@@ -63,5 +75,8 @@ nx.draw_networkx_edges(network_graph, pos, edge_color=edge_color_list)
 
 edge_labels = nx.get_edge_attributes(network_graph, "weight")
 nx.draw_networkx_edge_labels(network_graph, pos, edge_labels, font_size=6)
+
+plt.title("Stockholm metro map")
+plt.text(70, -92, 'Distances between edges represented in kilometres', fontsize=8)
 
 plt.show()
